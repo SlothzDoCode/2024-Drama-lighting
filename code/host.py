6e2ebd4bd3b229725743c9949c2305500bdfdd69
SlothzDoCode <146091658+SlothzDoCode@users.noreply.github.com>
2024-09-20 13:38:18 +0100

    Update host.py

--- code/host.py
+++ code/host.py
@@ -15,6 +15,21 @@ display = tk.Tk()
 
 threading.Thread(target=server)
 
+btn1 = tk.Button(
+    text="set 1",
+    width=9,
+    height=4,
+    command="")
+btn1.place(x=100,y=100)
+btn1.config(bg='#404040',fg='#FFFFFF')
+
+btn2 = tk.Button(text="set 2",
+    width=9,
+    height=4,
+    command="")
+btn2.place(x=200,y=100)
+btn2.config(bg='#404040',fg='#FFFFFF')
+
 display.config(background='#121212')
 display.attributes('-fullscreen', True)
 display.attributes('-topmost', True)
